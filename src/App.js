c72283253d0230230842f9c4f1dca84abdc2c392
Stephen Hukish <stephen.hukish@gmail.com>
2019-06-24 13:04:54 -0400

    adding todo list start`

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
+import {Page, Stack, Layout, Button, Card, Checkbox} from '@shopify/polaris';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todoList: {
+      todo1: false,
+      todo2: false,
+      todo3: false,
+      todo4: false,
+    },
+  };
+  render() {
+    const {todoList} = this.state;
+    return (
+      <Page title="React Todo List">
+        <Layout>
+          <Layout.Section>
+            <Stack alignment="trailing"><Button primary>Add todo item</Button></Stack>
+          </Layout.Section>
+          <Layout.Section>
+            <Card>
+              <ul>
+                <li><Checkbox id="todo1" name="todo1" label="Todo 1" checked={todoList.todo1} onChange={this.handleChange} /></li>
+                <li><Checkbox id="todo2" name="todo2" label="Todo 2" checked={todoList.todo2} onChange={this.handleChange} /></li>
+                <li><Checkbox id="todo3" name="todo3" label="Todo 3" checked={todoList.todo3} onChange={this.handleChange} /></li>
+                <li><Checkbox id="todo4" name="todo4" label="Todo 4" checked={todoList.todo4} onChange={this.handleChange} /></li>
+              </ul>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </Page>
+    );
+  }
+
+  handleChange = (value, id) => {
+    console.log(value, id);
+    this.setState((prevState) => ({
+      ...prevState,
+      todoList: {
+        ...prevState.todoList,
+        [id]: value,
+      }
+    }));
+  };
 }
 
 export default App;
